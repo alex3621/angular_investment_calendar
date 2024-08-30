c2794d19e0661a5b540e1030046f74b0909b0007
Alex Lin <chinyinalex.lin@gmail.com>
2024-08-29 23:39:05 -0400

    updated for an emitter

--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Output , EventEmitter} from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { type UserEntry } from './user-input.model';
 
 @Component({
   selector: 'app-user-input',
@@ -9,11 +10,23 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './user-input.component.css'
 })
 export class UserInputComponent {
+  @Output() data = new EventEmitter<UserEntry>();
   enteredInvestmentAmount = 0
   enteredAnnualInvestment = 0
   expectedReturn = 0
   duration = 0
   onSubmit(){
-    console.log('entered :'+ this.enteredAnnualInvestment + this.duration )
+    console.log({
+      enteredInvestmentAmount :this.enteredInvestmentAmount,
+      enteredAnnualInvestment : this. enteredAnnualInvestment,
+      expectedReturn :this.expectedReturn,
+      duration : this.duration 
+    })
+    this.data.emit({
+      enteredInvestmentAmount :this.enteredInvestmentAmount,
+      enteredAnnualInvestment : this. enteredAnnualInvestment,
+      expectedReturn :this.expectedReturn,
+      duration : this.duration 
+    }) 
   }
 }
