a3411325aa3c2896aae53ba577884e13e9c372aa
Alex Lin <chinyinalex.lin@gmail.com>
2024-08-29 23:57:27 -0400

    added logic and function for calculation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,16 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserInputComponent } from './user-input/user-input.component';
 import { InvestmentResultsComponent } from './investment-results/investment-results.component';
+import { UserEntry } from './user-input/user-input.model';
 
+interface YearlyData {
+  year: number;
+  interest: number;
+  valueEndOfYear: number;
+  annualInvestment: number;
+  totalInterest: number;
+  totalAmountInvested: number;
+}
 
 @Component({
   selector: 'app-root',
@@ -10,4 +19,34 @@ import { InvestmentResultsComponent } from './investment-results/investment-resu
   templateUrl: './app.component.html',
   imports:[HeaderComponent, UserInputComponent, InvestmentResultsComponent]
 })
-export class AppComponent {}
+export class AppComponent {
+  private result: YearlyData[] = [];
+  calculated=false;
+  submitted(data : UserEntry)
+  {
+    this.result = this.calculateInvestmentResults(data.enteredInvestmentAmount, data.enteredAnnualInvestment, data.expectedReturn, data.duration);
+    this.calculated=true
+  }
+  calculateInvestmentResults(initialInvestment : number, annualInvestment:number, expectedReturn:number,duration:number ) {
+    const annualData = [];
+    let investmentValue = initialInvestment;
+  
+    for (let i = 0; i < duration; i++) {
+      const year = i + 1;
+      const interestEarnedInYear = investmentValue * (expectedReturn / 100);
+      investmentValue += interestEarnedInYear + annualInvestment;
+      const totalInterest =
+        investmentValue - annualInvestment * year - initialInvestment;
+      annualData.push({
+        year: year,
+        interest: interestEarnedInYear,
+        valueEndOfYear: investmentValue,
+        annualInvestment: annualInvestment,
+        totalInterest: totalInterest,
+        totalAmountInvested: initialInvestment + annualInvestment * year,
+      });
+    }
+  
+    return annualData;
+  }
+}
---
+++ src/app/user-input/user-input.model.ts
@@ -0,0 +1,6 @@
+export interface UserEntry {
+    enteredInvestmentAmount :number,
+    enteredAnnualInvestment : number,
+    expectedReturn:number,
+    duration:number
+  }
